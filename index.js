0491679bb044928446e0457213ddefd69f5fd628
Irfan Gul <irfangul.9@gmail.com>
2022-05-05 11:30:14 +0530

    This is the Version 1.

---
+++ index.js
@@ -0,0 +1,20 @@
+var password=document.getElementById("password");
+
+// Function to Generate Password
+ function genPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ }
+
+//  Function to Copy Password
+function copyPassword() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  document.execCommand("copy");  
+}
